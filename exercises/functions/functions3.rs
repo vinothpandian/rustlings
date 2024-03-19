c5ead9f476a6e8ad58736bdf55da295230b31b68
Vinoth Pandian <vinothpandian@users.noreply.github.com>
2024-03-19 10:42:05 -0400

    until 32.3%

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -3,14 +3,14 @@
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    call_me();
+    call_me(None);
 }
 
-fn call_me(num: u32) {
-    for i in 0..num {
+fn call_me(num: Option<i32> ) {
+    for i in 0..num.unwrap_or(2) {
         println!("Ring! Call number {}", i + 1);
     }
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,12 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples( n: i32) -> i32 {
+    let cost = if n > 40 { 1 } else { 2 };
+
+    n * cost
+}
 
 // Don't modify this function!
 #[test]
